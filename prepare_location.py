d65cfcba3faf0f8e11920a5948eee8830ccb57f5
Leona Hammelrath <50407361+leona-ha@users.noreply.github.com>
2023-09-28 12:20:04 +0200

    Update folder structure

---
+++ prepare_location.py
@@ -0,0 +1,17 @@
+"""
+Python Adaptation of the AMPPS tutorial 
+
+Müller, S. R., Bayer, J. B., Ross, M. Q., Mount, J., Stachl, C., Harari, G. M., Chang, Y.-J., Le, H. (2022). 
+Analyzing GPS Data for Psychological Research: A Tutorial. Advances in Methods and Practices in Psychological Science, 5(2), 
+https://doi.org/10.1177/25152459221082680
+
+"""
+## Setup
+
+# import packages 
+import pandas as pd
+import datetime as dt
+from datetime import date
+
+
+
