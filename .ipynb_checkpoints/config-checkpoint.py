cbf8e140ec485c53aa1cf33e35c11b431302b490
Leona Hammelrath <50407361+leona-ha@users.noreply.github.com>
2024-08-23 09:52:46 +0200

    Update data merging

---
+++ .ipynb_checkpoints/config-checkpoint.py
@@ -0,0 +1,9 @@
+# configuration file for the TIKI project
+
+datapath = "/Volumes/Leonas_SSD/tiki_data/"
+raw_path = "/Volumes/Leonas_SSD/tiki_data/raw/"
+preprocessed_path = "/Volumes/Leonas_SSD/tiki_data/preprocessed/"
+redcap_path = "/Volumes/Leonas_SSD/tiki_data/redcap"
+
+proj_sheet = "1z8LZJBBMzzAmiXIS47X8SLk-zSMwDIXSKPit4IlmfuE"
+credential_path = "/Users/leonahammelrath/FU_Psychoinformatik/Github/tiki_code/tiki-399609-97e6a6cac550.json"
