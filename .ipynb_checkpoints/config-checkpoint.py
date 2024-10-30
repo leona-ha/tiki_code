6945a32d99976c5fd52815d08fd62668c288db13
Leona Hammelrath <50407361+leona-ha@users.noreply.github.com>
2024-10-30 10:56:53 +0100

    minor updates

--- .ipynb_checkpoints/config-checkpoint.py
+++ .ipynb_checkpoints/config-checkpoint.py
@@ -1,9 +1,9 @@
 # configuration file for the TIKI project
 
-datapath = "/Volumes/Leonas_SSD/tiki_data/"
-raw_path = "/Volumes/Leonas_SSD/tiki_data/raw/"
-preprocessed_path = "/Volumes/Leonas_SSD/tiki_data/preprocessed/"
-redcap_path = "/Volumes/Leonas_SSD/tiki_data/redcap"
-
+datapath = "/Volumes/Leonas_SSD/tiki_data"
 proj_sheet = "1z8LZJBBMzzAmiXIS47X8SLk-zSMwDIXSKPit4IlmfuE"
-credential_path = "/Users/leonahammelrath/FU_Psychoinformatik/Github/tiki_code/tiki-399609-97e6a6cac550.json"
+credential_path = "/Users/leonahammelrath/Documents/GitHub/tiki_code/notebooks/tiki-399609-b68b5ade4dfb.json"
+preprocessed_path = "/Volumes/Leonas_SSD/tiki_data/preprocessed"
+raw_path = "/Volumes/Leonas_SSD/tiki_data/raw"
+redcap_path = "/Volumes/Leonas_SSD/tiki_data/redcap"
+drivesheet_url = "https://docs.google.com/spreadsheets/d/1z8LZJBBMzzAmiXIS47X8SLk-zSMwDIXSKPit4IlmfuE"
